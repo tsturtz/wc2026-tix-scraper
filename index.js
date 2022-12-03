69116f9ca8b7dac7c1962a3edc24dfc73ad267c8
Taylor Sturtz <tsturtz@blizzard.com>
2022-12-02 18:10:02 -0800

    should be good to go now

---
+++ index.js
@@ -0,0 +1,89 @@
+const { chromium } = require('playwright');
+const { expect } = require('@playwright/test');
+const { IncomingWebhook } = require('@slack/webhook');
+
+const url = process.env.SLACK_WEBHOOK_URL;
+
+// Initialize slack webhook
+const webhook = new IncomingWebhook(url);
+
+const sendMessage = async () => {
+  // Ping 3x cause wake up
+  await webhook.send({"text": '<@U0M3LEFK4>'})
+  .then((res) => { console.log('Pinged successfully: ', res) })
+  .catch((err) => { console.log('Error pinging: ', err) });
+  await webhook.send({"text": '<@U0M3LEFK4>'})
+  .then((res) => { console.log('Pinged successfully: ', res) })
+  .catch((err) => { console.log('Error pinging: ', err) });
+  await webhook.send({"text": '<@U0M3LEFK4>'})
+  .then((res) => { console.log('Pinged successfully: ', res) })
+  .catch((err) => { console.log('Error pinging: ', err) });
+
+  // Send the deets
+  await webhook.send({
+    "blocks": [
+      {
+        "type": "header",
+        "text": {
+          "type": "plain_text",
+          "text": "2026 World Cup tickets alert!",
+        }
+      },
+      {
+        "type": "image",
+        "title": {
+          "type": "plain_text",
+          "text": "IT'S HAPPENING!",
+        },
+        "image_url": "https://media.tenor.com/igRLRCPgjvEAAAAC/its-happening-michael-scott.gif",
+        "alt_text": "IT'S HAPPENING!"
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": ":rotating_light: Something changed on the tickets page (https://www.fifa.com/tournaments/mens/worldcup/canadamexicousa2026/tickets)! Tickets _might_ now be on sale! :rotating_light:",
+        }
+      },
+      {
+        "type": "actions",
+        "elements": [
+          {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "text": "Go check the site",
+            },
+            "style": "primary",
+            "url": "https://www.fifa.com/tournaments/mens/worldcup/canadamexicousa2026/tickets",
+          }
+        ]
+      }
+    ]
+  }).then((res) => {
+    console.log('Message sent to slack. Response: ', res);
+  }).catch((err) => {
+    console.log('There was an error sending a message to slack: ', err)
+  });
+}
+
+(async () => {
+  const browser = await chromium.launch({ headless: true });
+  const page = await browser.newPage();
+
+  await page.goto('https://www.fifa.com/tournaments/mens/worldcup/canadamexicousa2026/tickets');
+  await page.getByRole('button', { name: 'I\'m OK with that' }).click();
+
+  try {
+    await expect(page.getByText('Information regarding ticketing for the FIFA World Cup 2026™ will be available in due course.')).toBeVisible();
+    await expect(page.getByText('Please check this page regularly for further updates.')).toBeVisible();
+
+    console.log('Just checked the site and it is unchanged.');
+  } catch (e) {
+    console.error('Playwright assertion failure: ', e);
+
+    sendMessage();
+  }
+
+  await browser.close();
+})();
---
+++ run-on-schedule.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while :
+do
+  node index.js
+  sleep 10
+done
